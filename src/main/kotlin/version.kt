4a9232e111ea3c4187d0c45fa7a4781f1c86e9f7
Mgazul <mgazul@163.com>
2019-09-08 16:12:41 +0800

    Update version.kt

--- src/main/kotlin/version.kt
+++ src/main/kotlin/version.kt
@@ -18,7 +18,7 @@ enum class MinecraftVersion(
     val spigot: Boolean = false,
     val yarn: Boolean = false
 ) {
-    V1_14_4("1.14.4", "snapshot_nodoc_20190729", true, true, false),
+    V1_14_4("1.14.4", "snapshot_nodoc_20190719", true, true, false),
     V1_14_3("1.14.3", "snapshot_nodoc_20190719", true, true, false),
     V1_14_2("1.14.2", "stable_nodoc_53", false, true, false),
     V1_14_1("1.14.1", "stable_nodoc_51", false, true, false),
