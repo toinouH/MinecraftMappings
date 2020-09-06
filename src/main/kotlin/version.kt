e92dbfe624e18482d8d2887f02bf631199cd792c
Mgazul <mgazul@163.com>
2020-09-06 14:00:08 +0800

    1.16.2

--- src/main/kotlin/version.kt
+++ src/main/kotlin/version.kt
@@ -18,6 +18,7 @@ enum class MinecraftVersion(
     val yarn: Boolean = false,
     val spigot: Boolean = false
 ) {
+    V1_16_2("1.16.2", "snapshot_20200812", true, false, true),
     V1_16_2_YARN("1.16.2", null, false, true, true),
     V1_16_1_YARN("1.16.1", null, false, true, true),
     V1_16_1("1.16.1", "snapshot_20200625", false, true, true),
